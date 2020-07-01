2766ac23b808c8d7fdeb62654a142cce5864f0de
Ari Buenrostro <aribuenrostro@Aris-MacBook-Pro-2.local>
2020-07-01 17:33:20 -0500

    new SQL query

---
+++ .sql
@@ -0,0 +1,7 @@
+select dept_name from departments;
+select all from departments;
+select dept_no, dept_name from departments;
+select * from departments limit 10;
+commit;
+select dept_name, dept_no from departments;
+select all from dept_emp;
